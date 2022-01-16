c7396ea531f483b1bfff06a0b4324f137cb4978f
pspulkit <pulkitsoni2001@gmail.com>
2022-01-16 19:00:14 +0530

    intial commit

---
+++ lib/openscreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class openscreen extends StatelessWidget {
+  const openscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+
+        leading: const IconButton(onPressed: null, icon: Icon(Icons.menu),tooltip: 'Navigations menu',),
+        actions: [
+          Padding(
+          padding: EdgeInsets.only(right: 20.0) ,
+            child: const IconButton(onPressed: null, icon: Icon(Icons.search),),
+          ),
+          Padding(padding: EdgeInsets.only(right: 20.0),
+            child: const IconButton(onPressed: null,icon: Icon(Icons.shopping_cart),),
+          ),
+
+
+        ],
+        title: Text("The Village Mart",style: TextStyle(color: Colors.black),),backgroundColor: Colors.amberAccent),
+
+    );
+  }
+}
